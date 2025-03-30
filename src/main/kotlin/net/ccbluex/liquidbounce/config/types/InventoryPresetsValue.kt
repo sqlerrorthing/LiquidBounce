cc24da503b1f391781ef5ecef9566878ef02c193
sqlerrorthing <sqlerrorthing@gmail.com>
2025-03-30 11:33:15 +0800

    with predicate

--- src/main/kotlin/net/ccbluex/liquidbounce/config/types/InventoryPresetsValue.kt
+++ src/main/kotlin/net/ccbluex/liquidbounce/config/types/InventoryPresetsValue.kt
@@ -2,6 +2,7 @@ package net.ccbluex.liquidbounce.config.types
 
 import net.ccbluex.liquidbounce.features.inventoryPresets.InventoryPreset
 import net.ccbluex.liquidbounce.features.inventoryPresets.items.NonePresetItem
+import net.ccbluex.liquidbounce.features.inventoryPresets.items.PresetItem
 
 class InventoryPresetsValue : Value<List<InventoryPreset>>("InventoryPresets",
     defaultValue = emptyList(),
@@ -10,30 +11,52 @@ class InventoryPresetsValue : Value<List<InventoryPreset>>("InventoryPresets",
 ) {
     /**
      * Combines the [InventoryPreset] list into a single preset, applying priority rules:
-     * - Items from presets with a lower index in the list have the highest priority
+     * - Items from presets with a lower index in the list have the highest priority.
+     *   If the [predicate] does not contain this item,
+     *   then the item can be replaced with an item with a lower priority.
      * - [NonePresetItem] can be replaced by an item from a lower priority preset
      * - Non-[NonePresetItem] items are protected from being overwritten
      */
-    fun merged(): InventoryPreset? {
+    fun merged(predicate: (PresetItem) -> Boolean = { true }): InventoryPreset? {
         val presets = get()
 
         return when {
             presets.isEmpty() -> null
             presets.size == 1 -> presets[0]
-            else -> presets.merge()
+            else -> presets.merge(predicate)
         }
     }
 
     @Suppress("MagicNumber")
-    private fun List<InventoryPreset>.merge(): InventoryPreset {
+    private fun List<InventoryPreset>.merge(predicate: (PresetItem) -> Boolean): InventoryPreset {
         require(isNotEmpty()) {
             "At least one preset should be provided."
         }
 
         val mergedItems = Array(10) { index ->
-            this.firstNotNullOfOrNull { preset ->
-                preset.items.getOrNull(index)?.takeIf { it != NonePresetItem }
-            } ?: NonePresetItem
+            var selectedItem: PresetItem = NonePresetItem
+
+            @Suppress("LoopWithTooManyJumpStatements")
+            for (preset in this) {
+                if (index >= preset.items.size) {
+                    continue
+                }
+
+                val item = preset.items[index]
+
+                if (item == NonePresetItem) {
+                    if (selectedItem == NonePresetItem) {
+                        continue
+                    }
+                } else {
+                    if (predicate(item)) {
+                        selectedItem = item
+                        break
+                    }
+                }
+            }
+
+            selectedItem
         }
 
         val mergedThrows = this
