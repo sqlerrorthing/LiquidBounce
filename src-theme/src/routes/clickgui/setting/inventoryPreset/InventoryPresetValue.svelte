fd5d0ada85b16beeb7e62bf9c7e0110805edc44e
sqlerrorthing <148702857+sqlerrorthing@users.noreply.github.com>
2025-09-10 11:05:03 +0800

    remove useless component

--- src-theme/src/routes/clickgui/setting/inventoryPreset/InventoryPresetValue.svelte
+++ src-theme/src/routes/clickgui/setting/inventoryPreset/InventoryPresetValue.svelte
@@ -1,12 +1,17 @@
 <script lang="ts">
     import type {InventoryPresetValue, ModuleSetting} from "../../../../integration/types";
     import {spaceSeperatedNames} from "../../../../theme/theme_config";
-    import InventoryPreset from "./InventoryPreset.svelte";
     import {createEventDispatcher} from "svelte";
+    import ItemPreview from "./ItemPreview.svelte";
+    import PresetModal from "./config/PresetModal.svelte";
 
     export let setting: ModuleSetting;
 
+    let configuring = false
+
     const cSetting = setting as InventoryPresetValue;
+
+    $: preset = cSetting.value
 
     const dispatch = createEventDispatcher();
 
@@ -16,21 +21,38 @@
     }
 </script>
 
+<!-- svelte-ignore a11y-click-events-have-key-events -->
+<!-- svelte-ignore a11y-no-static-element-interactions -->
 <div class="setting">
     <div class="head">
         <span class="title">{$spaceSeperatedNames ? "Inventory Preset" : "InventoryPreset"}</span>
     </div>
 
     <div class="presets">
-        <InventoryPreset
-                bind:preset={cSetting.value}
-                on:change={handleChange}
-        />
+        <div class="preset" on:click={() => configuring = true}>
+            {#each preset.items as group, idx (idx)}
+                <ItemPreview bind:group />
+            {/each}
+        </div>
     </div>
 </div>
 
+{#if configuring}
+    <PresetModal
+            bind:preset
+            on:close={() => configuring = false}
+            on:change={handleChange}
+    />
+{/if}
+
 <style lang="scss">
   @use "../../../../colors.scss" as *;
+
+  .preset {
+    display: flex;
+    justify-content: space-between;
+    cursor: pointer;
+  }
 
   .setting {
     padding: 7px 0;
